40b697a4c2d5c835e2ac8251ce8d6dc3c4a8aa4e
MiranIbrahim <moeenuddin.miran@gmail.com>
2023-12-13 13:52:27 +0600

    first commit

---
+++ University/src/iit/oop/University_name.java
@@ -0,0 +1,12 @@
+package iit.oop;
+
+import firstProject.Student;
+
+public class University_name {
+	public static void main(String[] args) {
+		
+
+		Student s1 = new Student(12, "miran" , 10, true);
+		System.out.println(s1.getStudent_name());
+	}
+}
